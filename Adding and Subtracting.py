3061ac741a13457dab5f3c83608f74e293920d15
Leslie Nunez <lenny@Leslies-MacBook-Pro.local>
2025-03-21 23:32:19 -0400

    Added calculator scripts

---
+++ Adding and Subtracting.py
@@ -0,0 +1,17 @@
+Python 3.13.2 (v3.13.2:4f8bb3947cf, Feb  4 2025, 11:51:10) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> user_num = int(input('Enter a number:\n'))
+Enter a number:
+5
+>>> print('You entered:', user_num)
+You entered: 5
+>>> user_num2 = int(input('Enter a second number:\n'))
+Enter a second number:
+1
+>>> print('For your second number you entered:', user_num2)
+For your second number you entered: 1
+>>> print(user_num, '+', user_num2, 'is', user_num+user_num2)
+5 + 1 is 6
+>>> print(user_num, '-', user_num2, 'is', user_num-user_num2)
+5 - 1 is 4
+>>> 
---
+++ Multiply&Divide.py
@@ -0,0 +1,16 @@
+Python 3.13.2 (v3.13.2:4f8bb3947cf, Feb  4 2025, 11:51:10) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> user_num = int(input('Enter a number:\n'))
+Enter a number:
+10
+>>> print('You entered:', user_num)
+You entered: 10
+>>> user_num2 = int(input('Enter a second number:\n'))
+Enter a second number:
+2
+>>> print('Your second number entered is:', user_num2)
+Your second number entered is: 2
+>>> print(user_num ,'*', user_num2, 'is', user_num*user_num2)
+10 * 2 is 20
+>>> print(user_num ,'/', user_num2,'is', user_num/user_num2)
+10 / 2 is 5.0
